28915b1bd3bed97f477bcfdfa60e6440e9331f0e
Eric LaFontsee <elafontsee@gmail.com>
2020-09-16 13:46:33 -0500

    Basic server set up

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000; //lets heroku decide the path 
+var PORT = 3000; //lets heroku decide the path 
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var waitingList = [{
+var waiting = [{
         routeName: "eric",
         name: "Eric",
         id: 1,
@@ -23,33 +23,37 @@ var waitingList = [{
         id: 2,
     },
     {
-        routeName: "obiwankenobi",
-        name: "Obi Wan Kenobi",
-        role: "Jedi Master",
-        age: 55,
-        forcePoints: 1350
+        routeName: "miller",
+        name: "Miller",
+        id: 3,
+    },
+    {
+        routeName: "payton",
+        name: "Payton",
+        id: 4,
     }
 ];
 
-app.get("/", function(req, res) {
-    // res.send("Welcome to the Star Wars Page!")
-    res.sendFile(path.join(__dirname, "view.html"));
-});
+// app.get("/", function(req, res) {
+//     // res.send("Welcome to the Star Wars Page!")
+//     res.sendFile(path.join(__dirname, "view.html"));
+// });
 
 // Displays all characters
-app.get("/api/characters", function(req, res) {
-    return res.json(characters);
+app.get("/api/waiting", function(req, res) {
+    return res.json(waiting);
 });
 
+
 // Displays a single character, or returns false
-app.get("/api/characters/:character", function(req, res) {
-    var chosen = req.params.character;
+app.get("/api/waiting/:newwaiting", function(req, res) {
+    var current = req.params.newwaiting;
 
-    console.log(chosen);
+    console.log(current);
 
-    for (var i = 0; i < characters.length; i++) {
-        if (chosen === characters[i].routeName) {
-            return res.json(characters[i]);
+    for (var i = 0; i < waiting.length; i++) {
+        if (current === waiting[i].routeName) {
+            return res.json(waiting[i]);
         }
     }
 
@@ -57,19 +61,19 @@ app.get("/api/characters/:character", function(req, res) {
 });
 
 // Create New Characters - takes in JSON input
-app.post("/api/characters", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    var newcharacter = req.body;
+// app.post("/api/waiting", function(req, res) {
+//     // req.body hosts is equal to the JSON post sent from the user
+//     // This works because of our body parsing middleware
+//     var newWaiting = req.body;
 
-    console.log(newcharacter);
+//     console.log(newWaiting);
 
-    // We then add the json the user sent to the character array
-    characters.push(newcharacter);
+//     // We then add the json the user sent to the character array
+//     characters.push(newWaiting);
 
-    // We then display the JSON to the users
-    res.json(newcharacter);
-});
+//     // We then display the JSON to the users
+//     res.json(newWaiting);
+// });
 
 // Starts the server to begin listening
 // =============================================================
